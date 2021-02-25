5e164d1954143414a108a3c1bf09c8a346f95ebf
rohan-empire-k <rohan.bhatt@bacancy.com>
2021-02-25 17:16:50 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,100 @@
-import logo from './logo.svg';
+import { Button,Grid, Typography } from '@material-ui/core';
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+
+  const [curInput,setCurInput] = useState("");
+
+  const evalClickHandler = () =>
+  {
+    try {
+    let ans = eval(curInput);
+    setCurInput(ans);  
+    } catch (error) {
+      alert("Please Enter a valid expression")
+      setCurInput("")
+    }
+    
+  }
+
+  const clrClickHandler = () =>
+  {
+    setCurInput("");
+  }
+
+  const btnClickHandler = (val) =>
+  {
+    switch(val)
+    {
+      case val:
+        setCurInput(curInput + val);
+        break;
+      default:
+        alert("Will never be reached , hopefully!");
+
+    }
+  }
+
+  const sqlClickHandler = () =>
+  {
+    
+    try {
+      let ans = Math.pow(eval(curInput),2);
+      setCurInput(ans);
+    } catch (error) {
+      alert("Please Enter a Valid Expression")
+      setCurInput("")
+    }
+  }
+
+  const backClickHandler =() =>
+  {
+    let tmp = curInput;
+    let newstr = tmp.substr(0,(tmp.length-1))
+    setCurInput(newstr);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2 style={{display:'flex',justifyContent:'center'}}>Calculator Task</h2>
+      
+      <Typography variant="h4" justify="center" align="center" style={{marginBottom:'16px'}}>{curInput}</Typography>
+
+      <Grid container spacing={2} alignItems={'center'} justify={'center'}>
+        <Grid container spacing={1} justify={'center'} style={{marginBottom:'8px'}}>
+          <Grid item><Button variant={'contained'} color={'primary'} onClick={() => btnClickHandler("1")}>1</Button></Grid>
+          <Grid item><Button variant={'contained'} color={'primary'} onClick={() => btnClickHandler("2")}>2</Button></Grid>
+          <Grid item><Button variant={'contained'} color={'primary'} onClick={() => btnClickHandler("3")}>3</Button></Grid>
+          <Grid item><Button variant={'contained'} color={'secondary'} onClick={() => btnClickHandler("+")}>+</Button></Grid>
+        </Grid>
+        <Grid container spacing={1} justify={'center'} style={{marginBottom:'8px'}}>
+          <Grid item><Button variant={'contained'} color={'primary'} onClick={() => btnClickHandler("4")}>4</Button></Grid>
+          <Grid item><Button variant={'contained'} color={'primary'} onClick={() => btnClickHandler("5")}>5</Button></Grid>
+          <Grid item><Button variant={'contained'} color={'primary'} onClick={() => btnClickHandler("6")}>6</Button></Grid>
+          <Grid item><Button variant={'contained'} color={'secondary'} onClick={() => btnClickHandler("-")}>-</Button></Grid>
+        </Grid>
+        <Grid container spacing={1} justify={'center'} style={{marginBottom:'8px'}}>
+          <Grid item><Button variant={'contained'} color={'primary'} onClick={() => btnClickHandler("7")}>7</Button></Grid>
+          <Grid item><Button variant={'contained'} color={'primary'} onClick={() => btnClickHandler("8")}>8</Button></Grid>
+          <Grid item><Button variant={'contained'} color={'primary'} onClick={() => btnClickHandler("9")}>9</Button></Grid>
+          <Grid item><Button variant={'contained'} color={'secondary'} onClick={() => btnClickHandler("*")}>*</Button></Grid>
+        </Grid>
+        <Grid container spacing={1} justify={'center'} style={{marginBottom:'8px'}}>
+          <Grid item><Button variant={'contained'} color={'primary'} onClick={() => btnClickHandler("0")}>0</Button></Grid>
+          <Grid item><Button variant={'contained'} color={'primary'} onClick={() => btnClickHandler(".")}>.</Button></Grid>
+          <Grid item><Button variant={'contained'} color={'secondary'} onClick={() => evalClickHandler()}>=</Button></Grid>
+          <Grid item><Button variant={'contained'} color={'secondary'} onClick={() => clrClickHandler()}>C</Button></Grid>
+        </Grid>
+        <Grid container spacing={1} justify={'center'}>
+          <Grid item><Button variant={'contained'} color={'secondary'} onClick={() => btnClickHandler("/")}>/</Button></Grid>
+          <Grid item><Button variant={'contained'} color={'secondary'} onClick={()=> sqlClickHandler()}>SQR</Button></Grid>
+          <Grid item><Button variant={'contained'} color={'secondary'} onClick={()=> backClickHandler()}>X</Button></Grid>
+        </Grid>
+      </Grid>
+
+      
     </div>
   );
 }
